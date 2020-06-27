4f4b025eff20a235030894aab4aa0fcfe826d71c
jesusnboone <jesusnboone@gmail.com>
2020-06-26 21:46:11 -0500

    added temperature and time

--- assets/script.js
+++ assets/script.js
@@ -3,31 +3,39 @@ function weather() {
     var apiKey = "1805b1fb611b9541105f2e5a649fba0b"
   
     fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?q="
+        "https://api.openweathermap.org/data/2.5/weather?q="
         + city
         + "&appid="
         + apiKey
     )
 
-      .then(function(response) {
-        return response.json();
-      })
-
-      .then(function(response) {
-        console.log(response.city.name);
-
-            // Create a variable that will select the <div> where the GIF will be displayed
-            var responseContainerEl = document.querySelector('#response-container');
-            var citySearch = (response.city.name)
+    .then(function(response) {return response.json();})
+    
+    .then(function(response) {
+        if (city.length === 0) {
+            alert("Please enter a city!")
+        }
+        else {
+        console.log(response);
+        console.log(response.dt);
+        var temperatureContainerEl = document.querySelector('#temperature-container');
+        var cityContainerEl = document.querySelector('#city-container');
+        var dateContainerEl = document.querySelector('#date-container');
+        var nameContainerEl = document.querySelector('#name-container');
+        var date = Date(response.dt);
+        var citySearch = (response.name);
+        var temperature = (response.main.temp);
+        var temperatureF = Math.floor((temperature - 273.15) * 9 / 5 + 32);
       
-            // Empty out the <div> before we append a GIF to it
-      
-            var cityButton = document.createElement('button');
-            cityButton.innerHTML = citySearch
-      
-            // Append 'gifImg' to the <div>
-            responseContainerEl.appendChild(cityButton)
-        })
 
+        var cityButton = document.createElement('button');
+        cityButton.innerHTML = citySearch;
+        cityContainerEl.appendChild(cityButton);
+
+        dateContainerEl.innerHTML = date;
+        nameContainerEl.innerHTML = citySearch;
+        temperatureContainerEl.innerHTML = temperatureF + "°F";
+
+        }
+    })
 }
-  
